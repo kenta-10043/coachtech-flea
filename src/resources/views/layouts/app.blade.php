6f1d09587532bf5ac50488d0924dfbc3159f00c5
mori kenta <aquamoon1436@gmail.com>
2025-08-15 23:30:52 +0900

    一時保存

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -13,7 +13,67 @@
 </head>
 
 <body>
-    <header class="main-header"><img src="{{ 'storage/others/logo.svg' }}" alt="ロゴ"></header>
+    <header class="main-header">
+        <ul class="header__nav">
+            @if (Auth::check())
+                <li><img class="title" src="{{ 'storage/others/logo.svg' }}" alt="ロゴ"></li>
+
+                <li>
+                    <form action="">
+                        <input class="input__search" type="text" value="" placeholder="　　　　　なにをお探しですか？　　　　　">
+                    </form>
+                </li>
+
+                <li>
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button class="button__logout">ログアウト</button>
+                    </form>
+                </li>
+
+                <li>
+                    <form action="" method="post">
+                        @csrf
+                        <button class="button__mypage">マイページ</button>
+                    </form>
+                </li>
+
+                <li>
+                    <form action="" method="post">
+                        @csrf
+                        <button class="button__exhibition">出品</button>
+                    </form>
+                </li>
+            @else
+                <li><img class="title" src="{{ 'storage/others/logo.svg' }}" alt="ロゴ"></li>
+
+                <li>
+                    <form action="">
+                        <input class="input__search" type="text" value="" placeholder="　　　　　なにをお探しですか？　　　　　">
+                    </form>
+                </li>
+
+                <li>
+                    <button onclick="location.href='/login'" class="button__login">ログイン</button>
+                </li>
+
+                <li>
+                    <form action="" method="post">
+                        @csrf
+                        <button type="button" class="button__mypage">マイページ</button>
+                    </form>
+                </li>
+
+                <li>
+                    <form action="" method="post">
+                        @csrf
+                        <button class="button__exhibition">出品</button>
+                    </form>
+                </li>
+            @endif
+
+        </ul>
+    </header>
     <div class="container">
         @yield('content')
     </div>
